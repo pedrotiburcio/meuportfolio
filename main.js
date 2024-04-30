f45ec860abf9165aeafa9310e80f3155a7359d56
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2024-04-30 12:28:59 -0300

    setup, seções início e sobre

---
+++ main.js
@@ -0,0 +1,52 @@
+// document.addEventListener('DOMContentLoaded', function (){
+//     setTimeout(function() {
+//         // animação dos links
+//         document.getElementById('social-links').style.opacity = "1";
+//         document.getElementById('social-links').style.animation = "bounce";
+//         document.getElementById('social-links').style.animationDuration = "2s";
+//     }, 3500);
+
+
+// })
+
+$(document).ready(function (){
+        const menuLinks = $('.header__menu__item__link');
+    
+        setTimeout(function(){
+            $('#social-links').css("opacity", "1")
+            $('#social-links').css("animation", "bounce")
+            $('#social-links').css("animationDuration", "2s")
+        }, 2900)
+
+        // $(window).on('scroll', function() {
+        //     hideAllLinks()
+        //     if($(window).scrollTop() <= 1199) {
+        //         $('.home-link').addClass('header__menu__item__link--is-active')
+        //     }
+        //     else if($(window).scrollTop() <= 2398) {
+        //         $('.about-link').addClass('header__menu__item__link--is-active')
+        //     }
+        //     else if($(window).scrollTop() <= 3597) {
+        //         console.log('a')
+        //         $('.projects-link').addClass('header__menu__item__link--is-active')
+        //     }
+        //     else {
+        //         $('.contact-link').addClass('header__menu__item__link--is-active')
+        //     }
+        // })
+    
+        // for(let i = 0; i < menuLinks.length; i++) {
+        //     $(menuLinks[i]).on("click", function() {
+        //         hideAllLinks()
+        //         $(menuLinks[i]).addClass('header__menu__item__link--is-active')
+        //     })
+        // }
+
+});
+
+function hideAllLinks(){
+    const menuLinks = $('.header__menu__item__link');
+    for(let i = 0; i < menuLinks.length; i++) {
+        $(menuLinks[i]).removeClass('header__menu__item__link--is-active')
+    }
+}
