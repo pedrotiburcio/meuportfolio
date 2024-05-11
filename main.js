36ca6d8552a5c64ea5591fb049e33e283501a405
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2024-05-11 16:51:45 -0300

    adicionando design responsivo

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 $(document).ready(function (){
-        const menuLinks = $('.header__menu__item__link');
-    
+        // const menuLinks = $('.header__menu__item__link');
+
+        const hamburguerMenu = $('.menu-hamburguer');
+        const menuItems = $('.header__menu');
+
+        hamburguerMenu.on("click", function(){
+            menuItems.slideToggle(700);
+        })
+
+        $(window).on("resize", function() {
+            if($(window).width() > 767) {
+                menuItems.css("display", "flex");
+            }
+            else {
+                menuItems.css("display", "none");
+            }
+        })
+
+
         setTimeout(function(){
             // $('#social-links').css("opacity", "1")
             $('#social-links').css("animation", "bounce")
             $('#social-links').css("animationDuration", "2.2s")
         }, 1500)
+
+        // $(window).on('resize', function(){
+        //     if($(window).width() < 1234) {
+        //         changeAnimations();
+        //     }
+        // })
 
         // $(window).on('scroll', function() {
         //     hideAllLinks()
@@ -39,3 +62,21 @@ $(document).ready(function (){
 //         $(menuLinks[i]).removeClass('header__menu__item__link--is-active')
 //     }
 // }
+
+
+
+// function hideAnimations()
+function changeAnimations() {
+    const projectsInfosAnimation = $('.projects__project__site')
+    const projectsImagesAnimation = $('.projects__project__infos')
+    const aboutPictureAnimation = $('.about__animation')
+    const aboutTextAnimation = $('.about__infos__text')
+
+    for(let i = 0; i < projectsInfosAnimation.length; i++) {
+        $(projectsInfosAnimation[i]).attr('data-aos', 'fade-down')
+        $(projectsImagesAnimation[i]).attr('data-aos', 'fade-up')
+    }
+
+    $(aboutPictureAnimation).attr('data-aos', 'fade-down')
+    $(aboutTextAnimation).attr('data-aos', 'fade-up')
+}
