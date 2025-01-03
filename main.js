c98b5a0bbd52b4ec720a512e5d73947c065458b1
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2025-01-03 11:04:10 -0300

    updating date time dinamically

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 $(document).ready(function (){
-        // const menuLinks = $('.header__menu__item__link');
-
         const hamburguerMenu = $('.menu-hamburguer');
         const menuItems = $('.header__menu');
 
@@ -17,54 +15,15 @@ $(document).ready(function (){
             }
         })
 
-
         setTimeout(function(){
             $('#social-links').css("animation", "bounce")
             $('#social-links').css("animationDuration", "2.2s")
         }, 1500)
 
-        // $(window).on('resize', function(){
-        //     if($(window).width() < 1234) {
-        //         changeAnimations();
-        //     }
-        // })
-
-        // $(window).on('scroll', function() {
-        //     hideAllLinks()
-        //     if($(window).scrollTop() <= 1199) {
-        //         $('.home-link').addClass('header__menu__item__link--is-active')
-        //     }
-        //     else if($(window).scrollTop() <= 2398) {
-        //         $('.about-link').addClass('header__menu__item__link--is-active')
-        //     }
-        //     else if($(window).scrollTop() <= 3597) {
-        //         console.log('a')
-        //         $('.projects-link').addClass('header__menu__item__link--is-active')
-        //     }
-        //     else {
-        //         $('.contact-link').addClass('header__menu__item__link--is-active')
-        //     }
-        // })
-    
-        // for(let i = 0; i < menuLinks.length; i++) {
-        //     $(menuLinks[i]).on("click", function() {
-        //         hideAllLinks()
-        //         $(menuLinks[i]).addClass('header__menu__item__link--is-active')
-        //     })
-        // }
-
+        const date = new Date().getFullYear();
+        $('#footer-text').append(date);
 });
 
-// function hideAllLinks(){
-//     const menuLinks = $('.header__menu__item__link');
-//     for(let i = 0; i < menuLinks.length; i++) {
-//         $(menuLinks[i]).removeClass('header__menu__item__link--is-active')
-//     }
-// }
-
-
-
-// function hideAnimations()
 function changeAnimations() {
     const projectsInfosAnimation = $('.projects__project__site')
     const projectsImagesAnimation = $('.projects__project__infos')
